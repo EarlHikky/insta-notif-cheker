4b7d7df31eae6fafc81f314657f14412ada7f09b
EarlHikky <andrew.malofeev@gmail.com>
2023-09-19 12:08:41 +0300

    test

--- insta_checker.py
+++ insta_checker.py
@@ -2,23 +2,30 @@ import argparse
 import asyncio
 import logging
 import os
+import random
 import textwrap
 import time
 from datetime import datetime
 from pathlib import Path
 
-from aiogram import Dispatcher, types, Bot
+from aiogram import Dispatcher, Bot, types
 from dotenv import load_dotenv
 from instagrapi import Client
 from instagrapi.exceptions import LoginRequired, PleaseWaitFewMinutes
 from requests.exceptions import RetryError
+
+logging.basicConfig(level=logging.INFO,
+                    filename='insta_checker_log.log',
+                    filemode='w',
+                    format='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger()
 
 
 class LoginUserException(Exception):
     pass
 
 
-async def send_notifications(notifications: dict):
+async def send_notifications(notifications: dict) -> None:
     """Send a message to Telegram.
 
     Args:
@@ -64,44 +71,54 @@ async def check_new_notifications(last_check_timestamp: float) -> dict:
             logger.info('Re-login started')
             if login_user():
                 break
-
-    threads = cl.direct_threads(selected_filter='unread')
-    if threads:
-        for thread in threads:
-            notifications[thread.thread_title] = []
-            messages = thread.messages
-            for message in messages:
-                # message_timestamp = message.timestamp.astimezone(current_timezone).timestamp()
-                message_timestamp = message.timestamp.timestamp()
-                if message_timestamp >= last_check_timestamp and not message.is_sent_by_viewer:
-                    message_type = message.item_type
-                    if message_type == 'xma_story_share':
-                        notifications[thread.thread_title].append('Stories')
-                    elif message_type == 'xma_media_share':
-                        notifications[thread.thread_title].append(message.xma_share.preview_url.__str__())
-                    else:
-                        message_content = message.__getattribute__(message_type)
-                        if message_type == 'clip':
-                            notifications[thread.thread_title].append(message_content.video_url.__str__())
-                        elif message_type == 'text':
-                            notifications[thread.thread_title].append(message_content)
-            cl.direct_send_seen(int(thread.id))
+    try:
+        threads = cl.direct_threads(selected_filter='unread')
+    except LoginRequired:
+        login_user()
+    else:
+        if threads:
+            for thread in threads:
+                notifications[thread.thread_title] = []
+                messages = thread.messages
+                for message in messages:
+                    # message_timestamp = message.timestamp.astimezone(current_timezone).timestamp()
+                    message_timestamp = message.timestamp.timestamp()
+                    if message_timestamp >= last_check_timestamp and not message.is_sent_by_viewer:
+                        message_type = message.item_type
+                        if message_type == 'xma_story_share':
+                            notifications[thread.thread_title].append('Stories')
+                        elif message_type == 'xma_media_share':
+                            notifications[thread.thread_title].append(message.xma_share.preview_url.__str__())
+                        else:
+                            message_content = message.__getattribute__(message_type)
+                            if message_type == 'clip':
+                                notifications[thread.thread_title].append(message_content.video_url.__str__())
+                            elif message_type == 'text':
+                                notifications[thread.thread_title].append(message_content)
+                cl.direct_send_seen(int(thread.id))
 
     return notifications
 
 
 async def start_checking():
     while True:
+        global check_interval
         logger.info('Auto-check started')
         current_timestamp = datetime.now().timestamp()
+        min_seconds = 15 * 60
+        max_seconds = 2 * 60 * 60
+        check_interval = random.randint(min_seconds, max_seconds)
         last_check_timestamp = current_timestamp - check_interval
         try:
             notifications = await check_new_notifications(last_check_timestamp)
         except TimeoutError:
             await bot.send_message(chat_id=private_tg_id, text='Login troubles')
         else:
-            if notifications:
-                await send_notifications(notifications)
+            try:
+                if notifications:
+                    await send_notifications(notifications)
+            except Exception as e:
+                logger.error('Error occurred during send_notifications: %s' % e)
         await asyncio.sleep(check_interval)
 
 
@@ -111,7 +128,7 @@ async def start_bot():
     await dp.start_polling()
 
 
-def check_session():
+def check_session() -> bool:
     try:
         cl.get_timeline_feed()
     except LoginRequired:
@@ -130,9 +147,9 @@ def login_user():
     Attempts to login to Instagram using either the provided session information
     or the provided username and password.
     """
-    logger.info('start login user')
-
-    session = cl.load_settings(Path('session.json'))
+    logger.info('Start login user')
+    session_json = Path('session.json')
+    session = cl.load_settings(session_json)
 
     login_via_session = False
     login_via_pw = False
@@ -141,17 +158,15 @@ def login_user():
         try:
             cl.set_settings(session)
             cl.login(username, password)
-            if not check_session():
+            if not check_session():  # TODO
+                logger.info('Start new session with old uuid')
                 old_session = cl.get_settings()
 
                 # use the same device uuids across logins
                 cl.set_settings({})
                 cl.set_uuids(old_session['uuids'])
-
+                logger.info(f"Old uuid{old_session['uuids']}")
                 cl.login(username, password)
-                # cl.login(username, password,
-                #          verification_code=cl.totp_generate_code(seed))
-
             else:
                 login_via_session = True
 
@@ -165,11 +180,9 @@ def login_user():
         try:
             logger.info('Attempting to login via username and password. username: %s' % username)
             if cl.login(username, password):
-                # if cl.login(username, password,
-                #             verification_code=cl.totp_generate_code(seed)):
                 if check_session():
                     login_via_pw = True
-                    cl.dump_settings(Path('session.json'))
+                    cl.dump_settings(session_json)
         except Exception as e:
             logger.info('Couldn\'t login user using username and password: %s' % e)
 
@@ -179,26 +192,36 @@ def login_user():
     return True
 
 
+def get_client(country: str = '') -> Client:
+    client = Client()
+    client.delay_range = [1, 3]
+    # client.set_locale('de_DE')
+    # client.set_timezone_offset(1 * 60 * 60)
+    return client
+
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, filename='insta_checker_log.log', filemode='w',
+    logging.basicConfig(level=logging.INFO,
+                        filename='insta_checker_log.log',
+                        filemode='w',
                         format='%(asctime)s %(levelname)s %(message)s')
     logger = logging.getLogger()
     parser = argparse.ArgumentParser(description='Start checking insta notifications.')
     parser.add_argument('-v', '--verif_code', type=int, help='Verification code')
     parser.add_argument('--check_interval', type=int, default=3600, help='Interval for checking. 1 hour by default.')
-    verif_code = str(parser.parse_args().verif_code)
+    # verif_code = str(parser.parse_args().verif_code)
     check_interval = parser.parse_args().check_interval
 
-    current_timezone = datetime.now().astimezone().tzinfo
+    # current_timezone = datetime.now().astimezone().tzinfo
 
     load_dotenv()
     tg_bot_token = os.environ['TELEGRAM_BOT_API_TOKEN']
     private_tg_id = os.environ['PRIVATE_CHAT_ID']
     telegram_chat_id = os.environ['CHAT_ID']
-    # username, password = os.environ['INST_USER'], os.environ['INST_PASS']  # instagram user
-    seed = os.environ['SEED']  # 2FA seed
+    # username, password = os.environ['INST_USER_MEA'], os.environ['INST_PASS_MEA']  # instagram user
+    # seed = os.environ['SEED']  # 2FA seed
 
-    username, password = os.environ['INST_USER_MEA'], os.environ['INST_PASS_MEA']
+    username, password = os.environ['INST_USER'], os.environ['INST_PASS']
     # cl.login(username, password)
 
     # cl = Client()
@@ -207,10 +230,9 @@ if __name__ == '__main__':
     #
     # if check_session():
     #     cl.dump_settings('session.json')
-    cl = Client()
-    cl.delay_range = [1, 3]
+    cl = get_client()
+
     if login_user():
-        cl.delay_range = [1, 3]
         bot = Bot(tg_bot_token)
 
         loop = asyncio.get_event_loop()
